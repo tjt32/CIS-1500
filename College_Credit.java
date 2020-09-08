176d8b8a3491b6354766ca55852afd37cdb03a29
tjt32 <70613005+tjt32@users.noreply.github.com>
2020-09-08 12:49:36 -0400

    Update College_Credit.java

--- College_Credit.java
+++ College_Credit.java
@@ -4,7 +4,7 @@ public class College_Credit {
 	public static void main(String[] args) {
 		//Define the Variables
 		int x;
-		int y;
+		double y;
 		int z;	
 		//Outputs
 		@SuppressWarnings("resource")
@@ -13,18 +13,20 @@ public class College_Credit {
 	    System.out.println("Hello Person!?! my sole purpose is to serve college students to estimate their college cost. What is your name?"); 
 	    String name = ss.nextLine(); 
 	    System.out.println(name + ", What is the amount of credits you have passed"); 
-	    int degree = ss.nextInt();
+	    int credit = ss.nextInt();
 	    System.out.println(name + ", How many of these credits things do you need to get an associates degree");
+	    int degree = ss.nextInt();
+	    System.out.println(name + ", How many of these credits does one take per semester?");
 	    int pSemester = ss.nextInt();
 	    System.out.println(name + ", Wait you have to pay to expand your knowledge, tell me the cost per class");
 	    int cCredit = ss.nextInt();
 	    
-	    x = pSemester - degree;
-	    y = x / degree;
+	    x = degree - credit;
+	    y = x / pSemester;
 	    z = x * cCredit;
 	    
 	    System.out.println("Calculating...");
-	    System.out.println(name +", based on some super complicated math it will take one " + y + " semesters to finish your degree which will cost one $" + z + " + don't forget the paper tax I mean stamp act wait I mean tottaly not a scam college textbooks!!!");
+	    System.out.println(name +", based on some super complicated math it will take one " + (int)Math.ceil(y) + " semesters to finish your degree which will cost one $" + z + " + don't forget the paper tax I mean stamp act wait I mean tottaly not a scam college textbooks!!!");
 	    
 	    
 		
